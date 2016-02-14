e2bbf544fdbc2ae06032db026777d64c49654637
berwin <berwin7996@gmail.com>
2016-02-13 20:29:39 -0600

    finished?

--- source_js/script.js
+++ source_js/script.js
@@ -9,10 +9,10 @@ for(var i=0; i<sections.length; i++) {
 // smooth scrolling
 // http://jsfiddle.net/9SDLw/
 $('.header-button').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 500);
-    return false;
+  $('html, body').animate({
+    scrollTop: $( $(this).attr('href') ).offset().top
+  }, 500);
+  return false;
 });
 
 // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport/7557433#7557433
@@ -32,28 +32,32 @@ function isElInViewport(el) {
 }
 
 $(document).ready(function(){
-    $("#section-one-button").addClass("active");
+  $("#section-one-button").addClass("active");
 });
 
 var currentSection = 1;
 // document scroll handler
 $(document).on( 'scroll', function(){
 
-    var sectionOneVisible = isElInViewport(document.getElementById('section-one'));
-    var sectionTwoVisible = isElInViewport(document.getElementById('section-two'));
-    var sectionThreeVisible = isElInViewport(document.getElementById('section-three'));
+  var sectionOneVisible = isElInViewport(document.getElementById('section-one'));
+  var sectionTwoVisible = isElInViewport(document.getElementById('section-two'));
+  var sectionThreeVisible = isElInViewport(document.getElementById('section-three'));
+  var sectionFourVisible = isElInViewport(document.getElementById('section-four'));
 
-    console.log(currentSection);
+  console.log(currentSection);
 
-    if(sectionOneVisible){
-      currentSection = 1;
-    }
-    else if(sectionTwoVisible){
-      currentSection = 2;
-    }
-    else if(sectionThreeVisible){
-      currentSection = 3;
-    }
+  if(sectionOneVisible){
+    currentSection = 1;
+  }
+  else if(sectionTwoVisible){
+    currentSection = 2;
+  }
+  else if(sectionThreeVisible){
+    currentSection = 3;
+  }
+  else if(sectionFourVisible){
+    currentSection = 4;
+  }
 
     // this is ugly im sorry
 
@@ -89,7 +93,9 @@ $(document).on( 'scroll', function(){
       $("#section-three-button").removeClass("active");
     }
 
-});
+
+
+  });
 
 // cheating the loop since loop isn't working
 // $('video').on('ended', function () {
@@ -108,17 +114,43 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+  modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+/* carousel */
+
+$('#left-button').on('click', function() {
+  // alert('hi');
+  toggleSlide(-1);
+});
+$('#right-button').on('click', function() {
+  toggleSlide(1);
+});
+
+var curslide = 0;
+// direction = boolean value: true or false. If true, go to NEXT slide; otherwise go to PREV slide
+function toggleSlide(direction) {
+  $(".hideable").eq(curslide).addClass("hidden");
+  curslide += direction;
+  if (curslide >= 3) curslide = 0;
+  if (curslide < 0) curslide = 2;
+  $(".hideable").eq(curslide).removeClass("hidden");
+  
+
+
+  console.log(curslide);
 }
